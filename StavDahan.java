5541a2cda13cd57560215aa3e94183b7d1417053
Stav Dahan <stav0405@gmail.com>
2022-11-23 18:04:06 +0200

    change the content in StavDahan file

--- StavDahan.java
+++ StavDahan.java
@@ -1,7 +1,7 @@
 public class StavDahan {
     public static void main(String[] args) {
         for (int i = 1; i <= 10; i++) {
-            System.out.println("Stav Dahan " + i);
+            System.out.println("Stav Dahan loves devops " + i);
         }
     }
 }
